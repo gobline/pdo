65e8fdb9436e9fd6e9e43a78834692daf9043b40
Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
2015-12-03 10:17:34 +0100

    php-cs-fixer

--- src/LazyPdo.php
+++ src/LazyPdo.php
@@ -44,42 +44,49 @@ class LazyPdo extends \PDO
 
         return parent::beginTransaction();
     }
+
     public function commit()
     {
         $this->connect();
 
         return parent::commit();
     }
+
     public function errorCode()
     {
         $this->connect();
 
         return parent::errorCode();
     }
+
     public function errorInfo()
     {
         $this->connect();
 
         return parent::errorInfo();
     }
+
     public function exec($statement)
     {
         $this->connect();
 
         return parent::exec($statement);
     }
+
     public function getAttribute($attribute)
     {
         $this->connect();
 
         return parent::getAttribute($attribute);
     }
+
     public static function getAvailableDrivers()
     {
         $this->connect();
 
         return parent::getAvailableDrivers();
     }
+
     public function inTransaction()
     {
         if (!$this->isConnected) {
@@ -89,36 +96,42 @@ class LazyPdo extends \PDO
 
         return parent::inTransaction();
     }
+
     public function lastInsertId($name = null)
     {
         $this->connect();
 
         return parent::lastInsertId($name);
     }
+
     public function prepare($statement, $driver_options = array())
     {
         $this->connect();
 
         return parent::prepare($statement, $driver_options);
     }
+
     public function query($statement)
     {
         $this->connect();
 
         return parent::query($statement);
     }
+
     public function quote($string, $parameter_type = \PDO::PARAM_STR)
     {
         $this->connect();
 
         return parent::quote($string, $parameter_type);
     }
+
     public function rollBack()
     {
         $this->connect();
 
         return parent::rollBack();
     }
+
     public function setAttribute($attribute, $value)
     {
         $this->connect();
